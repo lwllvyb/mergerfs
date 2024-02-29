18dbe050d7d6f56dea1a9ea8b07bb9318d3c8cb7
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-29 12:38:57 -0600

    preload.c

--- tools/preload.c
+++ tools/preload.c
@@ -216,7 +216,7 @@ openat(int         dirfd_,
   if(fd == -1)
     return -1;
 
-  if(flags_ & (O_DIRECTORY|O_PATH))
+  if(flags_ & (O_DIRECTORY|O_PATH|O_TMPFILE))
     return fd;
   rv = fstat(fd,&st);
   if(rv == -1)
